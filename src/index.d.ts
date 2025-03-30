a8d7840e29719e5f4c169eb58a5830580aafb71e
dejanpls <68787353+dejanpls@users.noreply.github.com>
2025-03-30 09:06:44 +0200

    feat: implement typescript declaration file and copy it to dist/ after each build

---
+++ src/index.d.ts
@@ -0,0 +1,9 @@
+declare module 'pls-dropdown' {
+  /**
+   * Toggles the visibility of a dropdown list when a toggle element is clicked.
+   *
+   * @param toggleId The ID of the toggle element.
+   * @param listId The ID of the dropdown list element.
+   */
+  export default function dropdown(toggleId: string, listId: string): void;
+}
